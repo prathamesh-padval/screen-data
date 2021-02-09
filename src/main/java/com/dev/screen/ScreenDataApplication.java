79460c18381abe50d66bf280d0eefc04d4c77bcd
Prathamesh <dev.prathameshpadval@gmail.com>
2021-02-09 20:48:12 +0530

    Cors added

--- src/main/java/com/dev/screen/ScreenDataApplication.java
+++ src/main/java/com/dev/screen/ScreenDataApplication.java
@@ -2,6 +2,9 @@ package com.dev.screen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ScreenDataApplication {
@@ -10,4 +13,17 @@ public class ScreenDataApplication {
 		SpringApplication.run(ScreenDataApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("PUT", "POST",
+						"OPTIONS", "GET", "DELETE", "HEAD").allowedHeaders("*");
+
+//			WebMvcConfigurer.super.addCorsMappings(registry);
+			}
+		};
+	}
 }
--- src/main/java/com/dev/screen/controller/ScreenController.java
+++ src/main/java/com/dev/screen/controller/ScreenController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.dev.screen.service.ScreenService;
 
 @RestController
 @RequestMapping(value = "/api")
+@CrossOrigin("*")
 public class ScreenController {
 
 	@Autowired
